d953beefc074f789f25a0b2a9cbdaf1daec55897
Tiaguin061 <tiaguin180@gmail.com>
2022-10-19 13:26:22 -0300

    feat: Imported new routers

--- src/shared/infra/http/start/router.ts
+++ src/shared/infra/http/start/router.ts
@@ -1,2 +1,5 @@
 import 'modules/User/http/router/user.router';
 import 'modules/Roles/http/router/roles.router';
+import 'modules/Permission/http/router/permission.router';
+import 'modules/UserPermission/http/router/user-permission.router';
+import 'modules/UserCompany/http/router/user-company.router';
