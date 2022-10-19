46e9cc6005283ff84c3d1bc2a2d51c2188946d09
Tiaguin061 <tiaguin180@gmail.com>
2022-10-19 13:25:17 -0300

    feat: Created user company module

---
+++ src/modules/UserCompany/http/router/user-company.router.ts
@@ -0,0 +1,11 @@
+import Route from '@ioc:Adonis/Core/Route';
+
+import { CreateUserCompanyController } from '../../useCases/CreateUserCompany/controller';
+import { RemoveUserCompanyController } from '../../useCases/RemoveUserCompany/controller';
+
+Route.group(() => {
+  Route.post('create', new CreateUserCompanyController().handle).as('create-user-company').middleware('auth');
+
+  Route.delete('remove', new RemoveUserCompanyController().handle).as('remove-user-company').middleware('auth');
+}).prefix('api/user-company/');
+
---
+++ src/modules/UserCompany/useCases/CreateUserCompany/controller.ts
@@ -0,0 +1,78 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { findByRole } from 'src/modules/Roles/repositories/role-repositories';
+import { findById } from 'src/modules/User/repositories/user-repositories';
+import { findUserRoleByRoleIdAndUserId } from 'src/modules/User/repositories/user-role-repositories';
+import { prisma } from 'src/shared/infra/prisma/client';
+
+export class CreateUserCompanyController {
+  public async handle({ request, response }: HttpContextContract) {
+    const userLoggedId = request.user.id;
+    try {
+      const { user_id, owner_company_id } = request.all();
+
+      if(!user_id) {
+        throw new Error('Please enter user_id');
+      }
+
+      if(!owner_company_id) {
+        throw new Error('Please enter owner_company_id');
+      }
+
+      const userLogged = await findById(userLoggedId);
+
+      if (!userLogged) { 
+        throw new Error('User not found');
+      }
+
+      const adminRole = await findByRole('admin');
+
+      if (!adminRole) {
+        throw new Error('Admin role not found');
+      }
+
+      const userAdminRole = await findUserRoleByRoleIdAndUserId(adminRole.id, userLoggedId);
+
+      const company = await prisma.company.findFirst({
+        where: {
+          ownerId: owner_company_id
+        }
+      });
+
+      if(!company) {
+        throw new Error('Company does not exist');
+      }
+
+      if(!userAdminRole && company?.ownerId !== userLoggedId) {
+        throw new Error('You does not owner this company');
+      }
+
+      const userCompany = await prisma.userCompany.findFirst({
+        where: {
+          userId: user_id
+        }
+      });
+
+      if (userCompany) {
+        throw new Error('This user has company');
+      }
+
+      const userCompanyCreated = await prisma.userCompany.create({
+        data: {
+          userId: user_id,
+          companyId: company?.id,
+        },
+        include: {
+          user: true,
+          company: true,
+        }
+      });
+  
+      return response.status(201).json({
+        userCompany: userCompanyCreated,
+        userLogged,
+      });
+    } catch (error) {
+      return response.status(403).json({error: error.message});
+    }
+  }
+}
