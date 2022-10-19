c9f71b85af57424f3d2b04805e1916b2c8032310
Tiaguin061 <tiaguin180@gmail.com>
2022-10-19 13:25:47 -0300

    feat: Added new routers

--- src/modules/User/http/router/user.router.ts
+++ src/modules/User/http/router/user.router.ts
@@ -1,4 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route';
+import { AdminAuthViewAsClientController } from '../../useCases/AdminAuthViewAsClient/controller';
 
 import { AuthUserController } from '../../useCases/AuthUser/controller';
 import { CreateUserController } from '../../useCases/CreateUser/controller';
@@ -14,6 +15,10 @@ Route.group(() => {
 
   Route.delete('delete', new RemoveUserController().handle).as('delete').middleware('auth');
   Route.put('update', new UpdateUserController().user).as('update').middleware('auth');
+
+  Route.group(() => {
+    Route.post('view-as-client', new AdminAuthViewAsClientController().handle).as('view-as-client').middleware('auth');
+  }).prefix('/admin/')
 
   Route.get('/verify-email/:email', new EmailVerificationListingController().handle).as('verifyEmail');
 }).prefix('api/users/');
