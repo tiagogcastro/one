6c95a643cf83c340b7cbc85cff1624cf73b28d40
Tiaguin061 <tiaguin180@gmail.com>
2022-10-22 19:06:14 -0300

    feat: Importing routes and auth context

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
 
-import GlobalStyles from './styles/global';
-import lightCustom from './styles/themes/light';
 import { AuthProvider } from './context';
 import { AppRouter } from './routes';
+import GlobalStyles from './styles/global';
+import lightCustom from './styles/themes/light';
 
 function App() {
   return (
